82faa9b74bb9cba74e6fecefec368d2d0a60560c
ahmadteeb <ahmadelkhatib1998@gmail.com>
2020-09-26 21:00:06 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Ahmadteeb | Among US Auto Mute Bot for Discord
+
+This bot mute/unmute s players in a specific server's channel named 'amongus'.  
+It uses [Tesseract OCR](https://sourceforge.net/projects/tesseract-ocr/files/latest/download) which is a a commercial quality OCR engine originally developed at HP between 1985 and 1995. In 1995, this engine was among the top 3 evaluated by UNLV. 
+
+## Installation
+1- Create an application from [Discord Developers](https://discord.com/developers/applications)  
+2- Copy the application token to 'token.json'
+3- Start the bot using the compiled file 'bot.exe'  
+4- Start the bot from chat using the command mentioned
+
+## Disocrd Commands 
+* #start : starts the bot 
+
+## How it works
+The executable waits for the start command on the server's chat channel then waits for the game to start by detecting 'Impostor' or 'Crewmate' and immediately mutes everyone until a dead body is reported or an emergency meeting is called where it unmutes everyone after detecting 'who is the impostor' at the top. 
+
+## Known Isuues 
+* The bot mutes everyone if the player running the bot opened in-game chat
+* The bot might not work on screens with resolution other than 1920x1080 
---
+++ src/bot.py
@@ -0,0 +1,88 @@
+from discord.utils import get
+from discord.ext import tasks
+from discord.ext import commands
+import cv2
+import pytesseract
+import pyautogui
+import json
+
+
+pytesseract.pytesseract.tesseract_cmd = "OCR/tesseract.exe"
+screenWidth, screenHeight = pyautogui.size()
+
+if(screenHeight == 1080): #1920x1080 
+    Center = (100, 100, 1500, 300)
+    Center2 = (500, 100, 800, 90)
+    Left_Top = (30 , 15, 300, 78)
+# elif(screenHeight == 768): #1366x768
+#     Center = (100, 100, 1500, 300)
+#     Left_Top = (30 , 15, 300, 78)
+
+def Image2Text(Region, _Color=cv2.COLOR_BGR2GRAY, _2blackandwhite=True, debug=False):
+    pyautogui.screenshot("Temp/temp.png", region = Region)
+    screen_shot = cv2.imread('Temp/temp.png')
+
+
+global Mute_Member
+global Channel_ID
+
+client = commands.Bot(command_prefix = '#')
+
+@client.event
+async def on_ready():
+    print("Bot is Ready")
+
+@client.command()
+async def start(ctx):
+    await ctx.send("I'm Ready!")
+    global Channel_ID
+    Channel_ID = get(ctx.guild.channels, name = "amongus").id
+    await amongus()
+
+async def amongus():
+    NewGame = True
+    Mute_Member = False
+    while True:
+        if(NewGame):
+            CMorIM = Image2Text(Center,_2blackandwhite=False)
+            if("Impostor" == CMorIM or "Crewmate" == CMorIM):
+                for member in list(client.get_channel(Channel_ID).members):
+                    await member.edit(mute = True)
+                NewGame = False
+                Mute_Member = True
+                while True:
+                    if("TOTAL TASKS COMPLETED" == Image2Text(Left_Top, _2blackandwhite=False, _Color=cv2.COLOR_BGR2RGB)):
+                        break
+        
+        else:
+            if(Mute_Member):
+                while "wmngmg" != Image2Text(Center2):
+                    if("Uictoru" == Image2Text(Center,_2blackandwhite=False) or "Defeat" == Image2Text(Center,_2blackandwhite=False)):
+                        for member in list(client.get_channel(Channel_ID).members):
+                            await member.edit(mute = False)
+                        NewGame = True
+                        break
+
+                for member in list(client.get_channel(Channel_ID).members):
+                    await member.edit(mute = False)
+                Mute_Member = False
+            else:
+                while "wmngmg" == Image2Text(Center2):
+                    pass
+                while("TOTAL TASKS COMPLETED" != Image2Text(Left_Top, _2blackandwhite=False, _Color=cv2.COLOR_BGR2RGB)):
+                    if("Uictoru" == Image2Text(Center,_2blackandwhite=False) or "Defeat" == Image2Text(Center,_2blackandwhite=False)):
+                        for member in list(client.get_channel(Channel_ID).members):
+                            await member.edit(mute = False)
+                        NewGame = True
+                        break
+
+                for member in list(client.get_channel(Channel_ID).members):
+                    await member.edit(mute = True)
+                Mute_Member = True
+
+
+with open("token.json") as token_file:
+    token = json.load(token_file)["token"]
+
+client.run(token)
+
